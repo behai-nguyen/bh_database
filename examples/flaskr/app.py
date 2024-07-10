17870c3b46832ef780f0ab43c683de0a8a6fb72b
behai-nguyen <behai_nguyen@hotmail.com>
2024-07-10 22:05:52 +1000

    Enable either MySQL or PostgreSQL. Bug Fixed logger.
    Added decorator @atexit.register

--- examples/flaskr/app.py
+++ examples/flaskr/app.py
@@ -2,6 +2,12 @@
 App entry point.
 """
 
+import logging
+
+import atexit
+
+from bh_database.core import Database
+
 from flaskr import create_app
 
 from flaskr.controllers.employees_admin import search_form
@@ -11,3 +17,17 @@ app = create_app()
 @app.route("/")
 def index():
     return search_form()
+
+@atexit.register
+def app_shutdown():
+    from flaskr import retrieve_queue_listener
+
+    Database.disconnect()
+
+    logger = logging.getLogger('flaskr.example')
+
+    logger.info("FlaskR example is shutting down...")
+    logger.info("Logging queue listener will stop listening...")
+
+    listener = retrieve_queue_listener()
+    listener.stop()
